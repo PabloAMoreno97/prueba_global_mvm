3daf4b762fb83f12e8b0f43a71357be89cd76016
Pablo Moreno <pablo_and.moreno@uao.edu.co>
2025-09-27 16:47:34 -0500

    Added deletion to the database operations

--- src/database.py
+++ src/database.py
@@ -35,14 +35,33 @@ def get_employee_by_id(id):
     try:
         session = SessionLocal()
         employee = session.query(Employee).filter_by(id=id).first()
-        print(employee)
+        if employee:
+            return employee
+        else:
+            print(f"No employee was found with id: {id}")
+            return None
     except Exception as e:
-        print("Error while obtaining data for employee with ID {id}:", e)
+        print(f"Error while obtaining data for employee with ID {id}:", e)
+
+
+def delete_employee_by_id(id):
+    try:
+        session = SessionLocal()
+        employee = session.query(Employee).filter_by(id=id).first()
+        if employee:
+            session.delete(employee)
+            session.commit()
+            print(f"Deleted employee with id: {id}")
+        else:
+            print(f"No employee was found with id: {id}")
+    except Exception as e:
+        print(f"Error while deleting employee with id {id}:", e)
 
 
 if __name__ == "__main__":
 
     new_employee = create_random_employee()
-    # add_employee(new_employee)
+    add_employee(new_employee)
     employees_list = list_employees()
     employee = get_employee_by_id(13599)
+    delete_employee_by_id(35823)
